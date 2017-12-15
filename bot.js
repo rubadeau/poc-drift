bce3387c478446916048f8561e0b9931207124c4
Tristan Rubadeau <tristan@rubadeau.com>
2017-12-15 10:46:18 -0700

    Initial Commit

---
+++ bot.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bot = express();
+const bodyParser = require('body-parser');
+
+bot.use(bodyParser.json());
+bot.listen(process.env.PORT || 3000, () => console.log('POC Bot for Drift is listening on port 3000!'));
+
+bot.post('/', (req, res) => {
+  if (req.body.type === 'new_message') {
+    delete req.body.token;
+    delete req.body.orgId;
+    delete req.body.data.context;
+
+    console.log("HERE IS YOUR FIRST EVENT: \n");
+    console.log( req.body);
+  }
+  return res.send('ok')
+});
+
